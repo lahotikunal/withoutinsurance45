d45adf92123654bb32a6dd9091f64344520b22d8
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2020-10-28 10:15:52 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -102,6 +102,9 @@ rules.addEventListener('click', function() {
   document.getElementById('rules').style.display = 'none';
 
 });
+next_game.addEventListener('click',function(){
+  {location.href = "https://lahotikunal.github.io/insurance_sty/";}
+});
 //rules
 
 button.addEventListener('click',function()
