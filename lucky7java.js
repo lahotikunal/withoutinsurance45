da9347b8c2ea13f4099bd70fe070d1acb65877a4
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2021-03-08 09:58:22 +0530

    Update lucky7java.js

--- lucky7java.js
+++ lucky7java.js
@@ -12,15 +12,7 @@ function myRadioValue(dice) {
 }
 
 //slider code
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
-  bidValue = parseInt(this.value);
-}
-//slider code
 
 
 function myFunction()
